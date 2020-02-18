4d59a29e55de7a4b40cf045c21d5b4ade412b651
Jakub <jaqb91@outlook.com>
2020-02-18 15:09:59 +0100

    user login

--- index.js
+++ index.js
@@ -33,15 +33,48 @@ app.get('/', (req, res) => {
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
 
-    user.save((err, userData) => {
+    user.save((err, doc) => {
         if (err) return res.json({
             success: false,
             err
         })
         return res.status(200).json({
-            success: true
+            success: true,
+            userData: doc
         })
     })
+})
+
+app.post('/api/user/login', (req, res) => {
+    //find email
+    User.findOne({
+        email: req.body.email
+    }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found."
+            });
+        //compare password
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "Passowrd did not match."
+                })
+            }
+        })
+        //generate token
+        user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            res.cookie("vod_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true
+                })
+        })
+    });
+
 })
 
 app.listen(5000);
