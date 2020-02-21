4ec1172a80c94bc2db10e9784feb4e13733396ba
Jakub <jaqb91@outlook.com>
2020-02-21 17:32:37 +0100

    heroku

--- server/index.js
+++ server/index.js
@@ -26,6 +26,11 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+    res.json({
+        "hello": "My first deploy."
+    })
+})
 
 app.get("/api/user/auth", auth, (req, res) => {
     res.status(200).json({
