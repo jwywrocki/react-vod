0fa6f353386c5d5616b64f337f6b08dfd0c9b1da
Jakub <jaqb91@outlook.com>
2020-02-25 01:17:17 +0100

    added register page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import About from './about';
 import Login from './register_login'
+import Register from './register_login/register'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/about" component={About} />
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Switch>
     </div>
   );
--- client/src/components/register_login/index.js
+++ client/src/components/register_login/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/user_actions'
+import { Link } from 'react-router-dom';
 
 class RegisterLogin extends Component {
     state = {
@@ -51,60 +52,73 @@ class RegisterLogin extends Component {
     render() {
         return (
             <div className="container">
-                <h2>Zaloguj się</h2>
-                {this.state.errors.length > 0 && (
-                    <div>
-                        {this.displayErrors(this.state.errors)}
-                    </div>
-                )}
                 <div className="row">
-                    <form className="col s6" onSubmit={event => this.submitForm(event)}>
+                    <h2 className="col s12 offset-s3">Zaloguj się</h2>
+                </div>
+                <div className="row">
+                    {this.state.errors.length > 0 && (
+                        <div className="col s12 offset-s3">
+                            {this.displayErrors(this.state.errors)}
+                        </div>
+                    )}
+                </div>
+                <div className="row">
+                    <form className="col s6 offset-s3" onSubmit={event => this.submitForm(event)}>
                         <div className="row">
-                            <div className="input-field col s6">
+                            <div className="input-field col s12">
                                 <input
-                                    name="email"
-                                    value={this.state.email}
-                                    onChange={e => this.handleChange(e)}
                                     id="email"
                                     type="email"
                                     className="validate"
+                                    name="email"
+                                    value={this.state.email}
+                                    onChange={e => this.handleChange(e)}
                                 />
-                                <label htmlFor="email">Email</label>
+                                <label className="active" htmlFor="email">Email</label>
                                 <span
                                     className="helper-txt"
-                                    data-error="Type a right email"
-                                    data-success="Correct"
+                                    data-error="wrong"
+                                    data-success="right"
                                 />
                             </div>
                         </div>
                         <div className="row">
-                            <div className="input-field col s6">
+                            <div className="input-field col s12">
                                 <input
-                                    name="password"
-                                    value={this.state.password}
-                                    onChange={e => this.handleChange(e)}
                                     id="password"
                                     type="password"
                                     className="validate"
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={e => this.handleChange(e)}
                                 />
-                                <label htmlFor="email">Password</label>
+                                <label className="active" htmlFor="password">Password</label>
                                 <span
                                     className="helper-txt"
-                                    data-error="Wrong"
-                                    data-success="Correct"
+                                    data-error="wrong"
+                                    data-success="right"
                                 />
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col s6">
+                            <div className="col s12">
                                 <button
-                                    className="btn waves-effect blue lighten-2"
+                                    className="btn waves-effect dark-green lighten-2"
                                     type="submit"
                                     name="action"
                                     onClick={this.submitForm}
                                 >
-                                    Login
+                                    Log in
+                                </button>&nbsp;&nbsp;
+                                <Link to="/register">
+                                    <button
+                                        className="btn waves-effect dark-green lighten-2"
+                                        type="submit"
+                                        name="action"
+                                    >
+                                        Sign up
                                 </button>
+                                </Link>
                             </div>
                         </div>
                     </form>
