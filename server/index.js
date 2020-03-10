a19f26054f1bde8c670035197023ab98ab402da5
Jakub <jaqb91@outlook.com>
2020-03-10 12:17:18 +0100

    server update

--- server/index.js
+++ server/index.js
@@ -29,6 +29,15 @@ app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
 
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    });
+}
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
