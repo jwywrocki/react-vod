646f6d9c4bbe3704b163e0b167802af12e0efb23
Jakub <jaqb91@outlook.com>
2020-03-12 23:14:33 +0100

    fetch movies from api

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,11 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import Auth from "../actions/auth";
 
-import Home from "./Home/Home.js";
-import Login from "./Login/Login.js";
-import Register from "./Register/Register.js";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
 import Nav from "./Nav/Nav";
-import Footer from "./Footer/Footer"
+import Footer from "./Footer/Footer";
+import MovieDetails from "./Details/MovieDetails";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/" component={Auth(Home, null)} />
           <Route exact path="/login" component={Auth(Login, false)} />
           <Route exact path="/register" component={Auth(Register, false)} />
+          <Route exact path="/movie/:movieId" component={Auth(MovieDetails, null)} />
         </Switch>
       </div>
       <Footer />
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,18 +1,33 @@
-import React, { useEffect } from 'react'
-import { API_KEY, API_URL } from '../Config'
+import React, { useEffect, useState } from 'react'
+import { API_KEY, API_URL, IMAGE_URL } from '../Config'
 
 import { Typography, Row } from 'antd';
+import List from './List';
 const { Title } = Typography;
 
 function Home() {
 
+    const [Movies, setMovies] = useState([])
+    const [CurrentPage, setCurrentPage] = useState(0)
     useEffect(() => {
-        fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=pl&page=1`)
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=pl&page=1`
+        fetchMovies(endpoint)
+    }, [])
+
+    const fetchMovies = (path) => {
+        fetch(path)
             .then(response => response.json())
             .then(response => {
                 console.log(response)
+                setMovies([...Movies, ...response.results])
+                setCurrentPage(response.page)
             })
-    }, [])
+    }
+
+    const handleClick = () => {
+        let endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=pl&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint)
+    }
 
     return (
         <div style={{ width: '100%', margin: 0 }}>
@@ -20,13 +35,20 @@ function Home() {
                 <Title level={2}>Popularne filmy</Title>
                 <hr />
                 <Row gutter={[16, 16]}>
-
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <List
+                                image={movie.poster_path && `${IMAGE_URL}w500${movie.poster_path}`}
+                                movieId={movie.id}
+                            />
+                        </React.Fragment>
+                    ))}
 
                 </Row>
 
                 <br />
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <button>Załaduj więcej</button>
+                    <button onClick={handleClick}>Załaduj więcej</button>
                 </div>
             </div>
         </div>
