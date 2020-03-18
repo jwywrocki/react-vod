80d4aa8d9976241d21eac2e2804e7c8348296c22
Jakub <jaqb91@outlook.com>
2020-03-18 01:44:03 +0100

    security update

--- client/src/components/Config.js
+++
@@ -1,5 +0,0 @@
-
-// API
-export const API_KEY = 'c8c61633afc7981aa0cdbc981e5020d7';
-export const API_URL = 'https://api.themoviedb.org/3/';
-export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
